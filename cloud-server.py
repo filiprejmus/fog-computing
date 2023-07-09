11203eb7a632fc456983a0f717f42447f12e9b76
filiprejmus <rejmusfilip@gmail.com>
2023-07-08 22:19:19 -0700

    Add queue for crashed client case

--- cloud-server.py
+++ cloud-server.py
@@ -12,6 +12,8 @@ class ServerTask:
         self.data = []
         self.response_data = 0
         self.client_idents = set({str.encode("sensor-1"), str.encode("sensor-2")})
+        # Messages to send keyed by client identity
+        self.messages_to_send = {ident: [] for ident in self.client_idents}
 
     def calculate_data_and_send(self, frontend):
         threading.Timer(15, self.calculate_data_and_send, [frontend]).start()
@@ -22,10 +24,10 @@ class ServerTask:
                 str(len(self.data)),
             )
         )
+        # Instead of sending, store the message in the dict
         for ident in self.client_idents:
-            print("Sending %s to %s", response_data, ident)
-
-            frontend.send_multipart([ident, response_data])
+            print("Storing %s for %s", response_data, ident)
+            self.messages_to_send[ident].append(response_data)
 
     def run(self):
         context = zmq.Context()
@@ -37,6 +39,10 @@ class ServerTask:
             ident, msg = frontend.recv_multipart()
             self.data.append(msg.decode())
             print("Cloud worker received %s from %s" % (msg, ident))
+            # Send any messages stored for this client
+            while self.messages_to_send[ident]:
+                response = self.messages_to_send[ident].pop(0)
+                frontend.send_multipart([ident, response])
 
         context.term()
 
